286fdc1983b6542aa7b84beff4ce26bc90ee2983
Matthew Manka <mrmanka0307@gmail.com>
2024-09-12 02:13:25 -0500

    removed unneeded import

--- .tests/utils/console-art.test.ts
+++ .tests/utils/console-art.test.ts
@@ -1,5 +1,4 @@
 import generateTextArt from '../../src/utils/console-art';
-import { M, A, C, U } from '../../src/utils/letters';
 
 describe('Console Art - Happy Paths', () => {
   test('Unit Test', () => {

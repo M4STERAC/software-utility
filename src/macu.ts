7da139a6f4a627da1023d624abb5235556cff48a
Matthew Manka <mrmanka0307@gmail.com>
2024-09-12 00:39:40 -0500

    Successfully executing command

--- src/macu.ts
+++ src/macu.ts
@@ -1,11 +1,12 @@
-import { program } from 'commander';
+import { Command } from 'commander';
 import create_godot_patch_release from './godot/create-godot-patch-release';
 
 const version = require('../package.json').version;
 
-console.log('Hello World!');
+const program = new Command();
 
 program
+  .name('macu')
   .version(version)
   .description('CLI to implement workflows for different projects')
 
@@ -14,3 +15,5 @@ program
   .alias('cgpr')
   .description('Creates a Godot release for working directory')
   .action(create_godot_patch_release);
+
+program.parse(process.argv);

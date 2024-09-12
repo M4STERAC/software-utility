6913bad31dad602903dfba54b61b152568254549
Matthew Manka <mrmanka0307@gmail.com>
2024-09-12 00:39:54 -0500

    moved into new folder

--- .tests/godot/create-godot-patch-release.test.ts
+++ .tests/godot/create-godot-patch-release.test.ts
@@ -1,8 +1,8 @@
 import create_godot_patch_release from '../../src/godot/create-godot-patch-release';
 
 describe('Happy Paths', () => {
-  test('Unit Test', async () => {
-    // const response = await create_godot_patch_release();
+  test('Unit Test', () => {
+    const response = create_godot_patch_release();
     // expect(response).toBeDefined();
   });
 });

4f20d4d8c979dad77fe843f02ffab00f075633c2
Matthew Manka <mrmanka0307@gmail.com>
2024-09-13 13:12:07 -0500

    updated test

--- .tests/godot/create-godot-patch-release.test.ts
+++ .tests/godot/create-godot-patch-release.test.ts
@@ -2,7 +2,7 @@ import create_godot_patch_release from '../../src/godot/create-godot-patch-relea
 
 describe('Happy Paths', () => {
   test('Unit Test', () => {
-    const response = create_godot_patch_release();
-    // expect(response).toBeDefined();
+    const response = create_godot_patch_release({ repository: 'EasyGSMOld', branch: 'v1.x.x' });
+    expect(response).toBeDefined();
   });
 });

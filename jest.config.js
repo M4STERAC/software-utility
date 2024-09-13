65590e63349d8f53196081d9d179d567757a5e84
Matthew Manka <mrmanka0307@gmail.com>
2024-09-13 13:10:33 -0500

    converted to .js

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,20 @@
-export default {
+module.exports = {
   globals: {
       'ts-jest': { tsconfig: '.tests/tsconfig.json' }
   },
+  moduleDirectories: [
+    'node_modules'
+  ],
   moduleFileExtensions: ['ts', 'js', 'json'],
   moduleNameMapper: {
       "^src\/(.*)$": "<rootDir>/src/$1",
       "^.mocks\/(.*)$": "<rootDir>/.mocks/$1",
       "^.data\/(.*)$": "<rootDir>/.data/$1",
   },
+  roots: [
+    '<rootDir>/.tests'
+  ],
+  testEnvironment: 'node',
   testMatch: [
     "**/__tests__/**/*.ts",
     "**/?(*.)(spec|test).ts"
@@ -18,7 +25,7 @@ export default {
       '^.+\\.sql$': '<rootDir>/.mocks/raw_loader.js'
   },
   transformIgnorePatterns: [
-      "/node_modules/"
+      "node_modules/(!?@octokit|chalk)"
   ],
   preset: 'ts-jest',
 //   setupFiles: ['<rootDir>/.tests/setEnvVars.ts'],

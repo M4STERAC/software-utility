25ff17788b2b2f6aea8107d16126034f5e8aba26
Matthew Manka <mrmanka0307@gmail.com>
2024-09-16 12:14:07 -0500

    added error handling for createGithubTag

--- src/godot/create-godot-patch-release.ts
+++ src/godot/create-godot-patch-release.ts
@@ -84,9 +84,13 @@ const getLatestRepoTag = async (GITHUB_USERNAME: string, GITHUB_REPOSITORY: stri
 };
 
 const createGithubTag = async (tag: string, GITHUB_USERNAME: string): Promise<void> => {
-  const message: string = `[macu cgpr]: Patch Release by ${GITHUB_USERNAME}`;
-  execSync(`git tag -a v${tag} -m "${message}"`);
-  execSync(`git push origin v${tag}`);
+  try {
+    const message: string = `[macu cgpr]: Patch Release by ${GITHUB_USERNAME}`;
+    execSync(`git tag -a v${tag} -m "${message}"`);
+    execSync(`git push origin v${tag}`);
+  } catch (err) {
+    throw `Failed to commit and/or push new tag: ${err}`;
+  }
 };
 
 export default async () => {

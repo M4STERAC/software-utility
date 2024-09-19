18edcb0e93d90a2db51c5e8c24feafa4f9a72a40
Matthew Manka <mrmanka0307@gmail.com>
2024-09-18 23:16:21 -0500

    Created new test for getting project.godot version

--- .tests/godot/utils/index.test.ts
+++ .tests/godot/utils/index.test.ts
@@ -1,13 +1,22 @@
-import { extractGodotVersion, readProjectGodot } from '../../../src/godot/utils/functions';
+import { extractGodotVersion, readProjectGodot, getLatestRepoTag, getSecrets } from '../../../src/godot/utils/functions';
 import * as path from 'path';
 
 jest.setTimeout(500);
+
 
 test('Extract Godot Version', async () => {
   const filepath = process.cwd() + path.sep + 'project.godot';
   const versionRegex = /config\/version="?(\d+\.\d+\.\d+)"?/;
   const project_godot = await readProjectGodot(filepath);
   const godot_version = await extractGodotVersion(project_godot, versionRegex);
+  
+  expect(godot_version[1]).toBe('0.0.0');
+});
 
-  expect(godot_version[1]).toBe('3.0.1');
+test('Get Latest Repository Tag', async () => {
+  const secrets = await getSecrets();
+  const REPOSITORY = 'godot-test-repo';
+  const tag = await getLatestRepoTag(secrets.username, REPOSITORY, secrets.personal_access_token);
+
+  expect(tag).toBeDefined();
 });
